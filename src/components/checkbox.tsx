5eaeb4398bc5f4c688871cb8354774d5b0c8bfc6
mottox2 <y.mottox2@gmail.com>
2022-06-17 14:58:37 +0900

    Update ui

---
+++ src/components/checkbox.tsx
@@ -0,0 +1,18 @@
+import { h } from 'preact'
+import { Checkbox as UICheckbox, Text } from "@create-figma-plugin/ui"
+
+export const Checkbox = (props: {
+  value: boolean;
+  onChange(value: boolean): void
+  label: string
+}) => {
+  const { value, onChange, label } = props
+  const handleValueChange = (newValue: boolean) => {
+    onChange(newValue)
+  }
+  return (
+    <UICheckbox onValueChange={handleValueChange} value={value}>
+      <Text>{label}</Text>
+    </UICheckbox>
+  )
+}
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,9 +1,10 @@
-import { h } from 'preact'
+import { Fragment, h } from 'preact'
 import { useState, useEffect } from 'preact/hooks'
 import { emit, on } from "@create-figma-plugin/utilities"
-import { Button, Container, render, VerticalSpace } from '@create-figma-plugin/ui'
+import { Button, Container, Divider, render, VerticalSpace, Text, Bold } from '@create-figma-plugin/ui'
 import { FilterInput } from './components/filter-input';
 import { MyDropdown } from './components/dropdown';
+import { Checkbox } from './components/checkbox';
 
 type Props = {
   families: string[],
@@ -22,12 +23,25 @@ const defaultFonts = {
   }
 }
 
+const Heading = (props: {
+  children: string
+}) => {
+  return <Fragment>
+    <VerticalSpace space="large" />
+    <Container space='medium'>
+      <Text><Bold>{props.children}</Bold></Text>
+    </Container>
+    <VerticalSpace space="small" />
+  </Fragment>
+}
+
 const App = ({ families, styles, editable: initialEditable }: Props) => {
   const [selectedFamily, setSelectedFamily] = useState<string | null>(defaultFonts.ja.family);
   const [selectedStyle, setSelectedStyle] = useState<string | null>(defaultFonts.ja.style);
   const [selectedFamily2, setSelectedFamily2] = useState<string | null>(defaultFonts.en.family);
   const [selectedStyle2, setSelectedStyle2] = useState<string | null>(defaultFonts.en.style);
   const [editable, setEditable] = useState<boolean>(initialEditable);
+  const [isDetail, setDetail] = useState(false)
 
   const apply = () => {
     emit("apply", {
@@ -50,18 +64,31 @@ const App = ({ families, styles, editable: initialEditable }: Props) => {
   })
 
   return (
-    <Container space='medium'>
-      <VerticalSpace space="extraSmall" />
-      <h4>Japanese</h4>
-      <FilterInput options={families} initialValue={selectedFamily} onChange={(newValue) => setSelectedFamily(newValue)} />
-      <MyDropdown options={styles[selectedFamily] ?? []} value={selectedStyle} onChange={(newValue) => setSelectedStyle(newValue)} />
-      <h4>English</h4>
-      <FilterInput options={families} initialValue={selectedFamily2} onChange={(newValue) => setSelectedFamily2(newValue)} />
-      <MyDropdown options={styles[selectedFamily2] ?? []} value={selectedStyle2} onChange={(newValue) => setSelectedStyle2(newValue)} />
-      <div style={{ position: 'absolute', bottom: 16, right: 16 }}>
-        <Button disabled={!editable} onClick={apply}>Apply</Button>
+    <Fragment>
+      <Heading>Japanese</Heading>
+      <Container space='extraSmall' style={{ display: 'flex' }}>
+        <div style={{ minWidth: '60%' }}>
+          <FilterInput options={families} initialValue={selectedFamily} onChange={(newValue) => setSelectedFamily(newValue)} />
+        </div>
+        <MyDropdown options={styles[selectedFamily] ?? []} value={selectedStyle} onChange={(newValue) => setSelectedStyle(newValue)} />
+      </Container>
+      <Heading>English</Heading>
+      <Container space='extraSmall' style={{ display: 'flex' }}>
+        <div style={{ minWidth: '60%' }}>
+          <FilterInput options={families} initialValue={selectedFamily2} onChange={(newValue) => setSelectedFamily2(newValue)} />
+        </div>
+        <MyDropdown options={styles[selectedFamily2] ?? []} value={selectedStyle2} onChange={(newValue) => setSelectedStyle2(newValue)} />
+      </Container>
+      <div style={{ position: 'absolute', bottom: 0, left: 0, right: 0, }}>
+        <Divider />
+        <div style={{ padding: 8, display: 'flex', alignItems: 'center', width: '100%' }}>
+          <div style={{ marginLeft: 4, marginRight: 'auto' }}>
+            <Checkbox value={isDetail} onChange={(val) => { setDetail(val) }} label={"Detail settings"} />
+          </div>
+          <Button disabled={!editable} onClick={apply}>Apply</Button>
+        </div>
       </div>
-    </Container>
+    </Fragment>
   );
 };
 
