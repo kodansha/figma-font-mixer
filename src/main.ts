deaf62167128178781da0511e78cd46e072ef62a
mottox2 <y.mottox2@gmail.com>
2022-06-23 13:29:57 +0900

    Add tests

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,12 @@
 import { emit, on, showUI, setRelaunchButton, saveSettingsAsync, loadSettingsAsync } from "@create-figma-plugin/utilities"
-import { Category, Fonts, Settings } from './types'
+import { Fonts, Settings } from './types'
+import { sortStyles, regexps, defaultSettings } from "./utils"
 
 const mapToObject = (map: Map<string, string[]>): Record<string, string[]> =>
   [...map].reduce((l, [k, v]) => Object.assign(l, { [k]: v }), {})
 
 const isSelectedTextNode = () => {
   return figma.currentPage.selection.length > 0 && figma.currentPage.selection.filter(node => node.type !== "TEXT").length === 0
-}
-
-const regexps: Record<Exclude<Category, "normal">, RegExp> = {
-  japanese: /(?:[々〇〻\u2E80-\u2FDF\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\u3041-\u3096\u30A1-\u30FC\uff1a-\uff20\u3001-\u301b]|[\uD840-\uD87F][\uDC00-\uDFFF])+/g,
-  kana: /[\u3041-\u3096\u30A1-\u30FC]+/g,
-  // ref: https://tama-san.com/kanji-regex/
-  kanji: /(?:[々〇〻\u2E80-\u2FDF\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF])+/g,
-  yakumono: /[\u3001-\u301b]+/g,
-  number: /\d+/g,
-}
-
-const defaultFonts: Record<'ja' | 'en', FontName> = {
-  ja: {
-    family: "Noto Sans JP",
-    style: "Medium"
-  },
-  en: {
-    family: "Inter",
-    style: "Medium",
-  }
-}
-
-const defaultSettings: Settings = {
-  fontMode: 'simple',
-  fonts: {
-    japanese: defaultFonts.ja,
-    kanji: defaultFonts.ja,
-    kana: defaultFonts.ja,
-    yakumono: defaultFonts.ja,
-    number: defaultFonts.en,
-    normal: defaultFonts.en,
-  }
-}
-
-const styleMapping: Record<string, number> = {
-  'Thin': 100,
-  'UltraLight': 200,
-  'ExtraLight': 200,
-  'Light': 300,
-  'Regular': 400,
-  'Medium': 500,
-  'SemiBold': 600,
-  'Bold': 700,
-  'ExtraBold': 800,
-  'Heavy': 800,
-  'Black': 900,
-  'W0': 100,
-  'W1': 200,
-  'W2': 250,
-  'W3': 300,
-  'W4': 400,
-  'W5': 500,
-  'W6': 600,
-  'W7': 700,
-  'W8': 800,
-  'W9': 900,
 }
 
 export default async () => {
@@ -82,15 +27,7 @@ export default async () => {
   const styles = mapToObject(styleMap)
   Object.keys(styles).map(key => {
     const values = styles[key];
-    const sorted = values.map(style => {
-      const match = Object.keys(styleMapping).find(pattern => style.includes(pattern))
-      const weight = styleMapping[match] ?? 400
-      const italic = style.includes('Italic')
-      return { label: style, weight, italic }
-    })
-      .sort((a, b) => a.weight - b.weight - (Number(b.italic) - Number(a.italic)) * 1000)
-      .map(({ label }) => label)
-    styles[key] = sorted
+    styles[key] = sortStyles(values)
   })
 
   const settings = await loadSettingsAsync(defaultSettings, 'fonts')
@@ -119,7 +56,6 @@ export default async () => {
       fonts: Fonts
     };
 
-    const { japanese, kanji, kana, yakumono, number, normal } = fonts
     const selected = figma.currentPage.selection[0]
     if (!selected || selected.type !== "TEXT") return
 
@@ -128,6 +64,7 @@ export default async () => {
       await figma.loadFontAsync(fontNames[i])
     }
 
+    const { japanese, kanji, kana, yakumono, number, normal } = fonts
     const categories = fontMode === 'simple' ? { japanese } : { kanji, kana, yakumono, number }
     selected.fontName = normal
 
---
+++ src/utils.ts
@@ -0,0 +1,69 @@
+import { Category, Settings } from './types'
+
+const defaultFonts: Record<'ja' | 'en', FontName> = {
+  ja: {
+    family: "Noto Sans JP",
+    style: "Medium"
+  },
+  en: {
+    family: "Inter",
+    style: "Medium",
+  }
+}
+
+export const defaultSettings: Settings = {
+  fontMode: 'simple',
+  fonts: {
+    japanese: defaultFonts.ja,
+    kanji: defaultFonts.ja,
+    kana: defaultFonts.ja,
+    yakumono: defaultFonts.ja,
+    number: defaultFonts.en,
+    normal: defaultFonts.en,
+  }
+}
+
+const styleMapping: Record<string, number> = {
+  'Thin': 100,
+  'UltraLight': 200,
+  'ExtraLight': 200,
+  'Light': 300,
+  'Regular': 400,
+  'Medium': 500,
+  'SemiBold': 600,
+  'Bold': 700,
+  'ExtraBold': 800,
+  'Heavy': 800,
+  'Black': 900,
+  'W0': 100,
+  'W1': 200,
+  'W2': 250,
+  'W3': 300,
+  'W4': 400,
+  'W5': 500,
+  'W6': 600,
+  'W7': 700,
+  'W8': 800,
+  'W9': 900,
+}
+
+export const sortStyles = (styles: string[]) => {
+  const sorted = styles.map(style => {
+    const match = Object.keys(styleMapping).find(pattern => style.includes(pattern))
+    const weight = styleMapping[match] ?? 400
+    const italic = style.includes('Italic')
+    return { label: style, weight, italic }
+  })
+    .sort((a, b) => a.weight - b.weight - (Number(b.italic) - Number(a.italic)) * 1000)
+    .map(({ label }) => label)
+  return sorted;
+}
+
+export const regexps: Record<Exclude<Category, "normal">, RegExp> = {
+  japanese: /(?:[々〇〻\u2E80-\u2FDF\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\u3041-\u3096\u30A1-\u30FC\uff1a-\uff20\u3001-\u301b]|[\uD840-\uD87F][\uDC00-\uDFFF])+/g,
+  kana: /[\u3041-\u3096\u30A1-\u30FC]+/g,
+  // ref: https://tama-san.com/kanji-regex/
+  kanji: /(?:[々〇〻\u2E80-\u2FDF\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF])+/g,
+  yakumono: /[\u3001-\u301b]+/g,
+  number: /\d+/g,
+}
